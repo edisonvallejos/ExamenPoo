af2495673f6dc7bde785a773f43e57a3f6ad739e
edisonvallejos <edison.vallejos018@ist17dejulio.edu.ec>
2025-11-17 11:26:08 -0500

    cuarto commit

--- src/main/java/com/mycompany/examenpoo/ExamenPOO.java
+++ src/main/java/com/mycompany/examenpoo/ExamenPOO.java
@@ -13,7 +13,7 @@ public class ExamenPOO {
 
     public static void main(String[] args) {
         Reportable e1 = new Desarrollador("Java", 0, "Israel", 18, 900, 2);
-        Reportable e2 = new Gerente("María", 40, 1500, 3, 4);
+        Reportable e2 = new Gerente(4, 1, "Maria", 30, 1500, 4);
 
         System.out.println(e1.generarReporte());
         System.out.println(e2.generarReporte());
